47afbfa47fe8b8805e92bb7a98f809d18f99359b
shubhamtakbhate99 <83940320+shubhamtakbhate99@users.noreply.github.com>
2021-05-23 17:19:41 +0530

    Update setup.py

--- totoisebotgoto_goal/setup.py
+++ totoisebotgoto_goal/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages = ['obstacle_avoidance'],
+    packages = ['totoisebotgoto_goal'],
     package_dir = {'': 'src'}
 )
 
