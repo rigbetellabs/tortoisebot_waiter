e754a70c72df4c7cef52ce70f2f6e00cedbdb034
shubhamtakbhate99 <83940320+shubhamtakbhate99@users.noreply.github.com>
2021-05-26 11:15:06 +0530

    Add files via upload

---
+++ tortoisebotgoto_goal/src/gui.py
@@ -0,0 +1,57 @@
+
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'button.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+import rospy
+from geometry_msgs.msg import Point
+from std_msgs.msg import float64
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def goal(self):
+        goal=Point()
+        self.goal.x=2
+        self.goal.y=3
+    #How to publish this goal to our maincode
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 300)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(30, 240, 341, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 89, 25))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton.setText(_translate("Dialog", "Table1"))
+
+
+if __name__ == "__main__":
+    import sys
+    rospy.init_node('gui')
+    pub1 = rospy.Publisher("goal", Point, queue_size=10)
+    pub.publish(goal)
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
