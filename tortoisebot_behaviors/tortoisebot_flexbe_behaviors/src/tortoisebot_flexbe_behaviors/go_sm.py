809dcb94a5f03d2180efbf6eb63ccd56f820e9e5
shubhamtakbhate99 <83940320+shubhamtakbhate99@users.noreply.github.com>
2021-05-30 11:27:37 +0530

    Add files via upload

---
+++ tortoisebot_behaviors/tortoisebot_flexbe_behaviors/src/tortoisebot_flexbe_behaviors/go_sm.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from tortoisebot_flexbe_states.move_base_state import MoveBaseState
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+from geometry_msgs.msg import Pose2D
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Sat May 29 2021
+@author: srt
+'''
+class GoSM(Behavior):
+	'''
+	hey
+	'''
+
+
+	def __init__(self):
+		super(GoSM, self).__init__()
+		self.name = 'Go'
+
+		# parameters of this behavior
+
+		# references to used behaviors
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		tab1_loc = Pose2D(x=-0.4, y=0.3, theta=1)
+		# x:30 y:275, x:130 y:275
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+		_state_machine.userdata.tab1_loc = tab1_loc
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:287 y:144
+			OperatableStateMachine.add('go',
+										MoveBaseState(),
+										transitions={'arrived': 'finished', 'failed': 'failed'},
+										autonomy={'arrived': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'waypoint': 'tab1_loc'})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
